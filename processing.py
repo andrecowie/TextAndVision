1e3dc1db8f41c3d048ec80a8557c2d8f7d9dc404
Andre Cowie <jpd3201@aut.ac.nz>
2017-10-25 17:40:15 +1300

    New Data

--- processing.py
+++ processing.py
@@ -1,15 +1,34 @@
 import json
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
+from textblob import TextBlob
 
 def main():
-	with open('data.json') as f:
-		data = json.load(f)
-	tweetCorpus = ", ".join([x['tweet'] for x in data])
-	fd = FreqDist(word_tokenize(tweetCorpus))
-	fd2 = FreqDist(tweetCorpus.split(' '))
+	with open('newdata.json') as f:
+		x = f.read()
+	data = json.loads(x)
+	allNes = " "
+	for x in data:
+		a = tokneiza(x[1]['attributes']['tweet']['S'])
+		allNes += " ".join(list(a[2]))
+	allNes.maketrans(" ","$")
+	allNes.maketrans(" ","#")
+	allNes.maketrans(" ", "@")
+	fd = FreqDist(word_tokenize(allNes))
 	print(fd.most_common(100))
-	print(fd2pp.most_common(100))
+
+
+def tokneiza(message):
+	if type(message) is not str:
+		message = str(message)
+	mess = TextBlob(message)
+	return mess.words, mess.tags, mess.noun_phrases, mess.sentiment
+
+	# tweetCorpus = ", ".join([x[1]['attributes']['tweet']['S'] for x in data])
+	# fd = FreqDist(word_tokenize(tweetCorpus))
+	# fd2 = FreqDist(tweetCorpus.split(' '))
+	# print(fd.most_common(100))
+	# print(fd2.most_common(100))
 
 
 
