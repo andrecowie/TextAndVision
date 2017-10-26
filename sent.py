ef25be2988855d7625496792a5c7a602d4cec9ef
tonypls <toe_knee_van@hotmail.com>
2017-10-26 21:20:36 +1300

    Added loadExample, pickle dun werk

--- sent.py
+++ sent.py
@@ -1,4 +1,5 @@
 import pandas
+from sklearn.externals import joblib
 import _pickle as cPickle
 from textblob import TextBlob
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -22,7 +23,7 @@ def traintest(messages):
     return [msg_train, msg_test, label_train, label_test]
 
 #Read Kaggle / Sentiment140 data, Sanders Twitter data and STS_Gold Twitter Data
-sent = pandas.read_csv('sent.csv',encoding='utf-8',error_bad_lines=False, nrows=5000)
+sent = pandas.read_csv('sent.csv',encoding='utf-8',error_bad_lines=False, nrows=4200)
 sent2 =pandas.read_csv('senti2.csv',encoding='utf-8',error_bad_lines=False)
 sent3 =pandas.read_csv('stsgold.csv',encoding='utf-8',error_bad_lines=False)
 
@@ -30,35 +31,37 @@ sent3 =pandas.read_csv('stsgold.csv',encoding='utf-8',error_bad_lines=False)
 frame = [sent, sent2, sent3]
 combi = pandas.concat(frame)
 
+
 def tonyFunction(msg_train, msg_test, label_train, label_test, info):
 
-    pipeline = Pipeline([
-        ('bow', CountVectorizer(analyzer=split_into_tokens)),  # strings to token integer counts can use #stop_words('english)
-        ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
-        ('classifier', MultinomialNB()),  # train on TF-IDF vectors w/ Naive Bayes classifier
-    ])
 
-    params = {
-        'tfidf__use_idf': (True, False),
-        }
+    # pipeline = Pipeline([
+    #     ('bow', CountVectorizer(analyzer=split_into_tokens)),  # strings to token integer counts can use #stop_words('english)
+    #     ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
+    #     ('classifier', MultinomialNB()),  # train on TF-IDF vectors w/ Naive Bayes classifier
+    # ])
+    #
+    # params = {
+    #     'tfidf__use_idf': (True, False),
+    #     }
+    #
+    # grid = GridSearchCV(
+    #     pipeline,  # pipeline from above
+    #     params,  # parameters to tune via cross validation
+    #     refit=True,  # fit using all available data at the end, on the best found param combination
+    #     n_jobs=1,  # number of cores to use for parallelization; -1 for "all cores"
+    #     scoring='accuracy',  # what score are we optimizing?
+    #     cv=StratifiedKFold(n_splits=5),  # what type of cross validation to use
+    # )
+    #
+    # nb_detector = grid.fit(msg_train, label_train)
+    # print('\nTest Results')
+    # print('\nMultinomial NaiveBayes '+info)
 
-    grid = GridSearchCV(
-        pipeline,  # pipeline from above
-        params,  # parameters to tune via cross validation
-        refit=True,  # fit using all available data at the end, on the best found param combination
-        n_jobs=1,  # number of cores to use for parallelization; -1 for "all cores"
-        scoring='accuracy',  # what score are we optimizing?
-        cv=StratifiedKFold(n_splits=5),  # what type of cross validation to use
-    )
-
-    nb_detector = grid.fit(msg_train, label_train)
-    print('\nTest Results')
-    print('\nMultinomial NaiveBayes '+info)
-
-    predictions = nb_detector.predict(msg_test)
-    print ('\nConfusion Matrix')
-    print (confusion_matrix(label_test, predictions))
-    print (classification_report(label_test, predictions))
+    # predictions = nb_detector.predict(msg_test)
+    # print ('\nConfusion Matrix')
+    # print (confusion_matrix(label_test, predictions))
+    # print (classification_report(label_test, predictions))
 
     #SVM Implementation
     pipeline_svm = Pipeline([
@@ -89,8 +92,15 @@ def tonyFunction(msg_train, msg_test, label_train, label_test, info):
     print (confusion_matrix(label_test, svm_detector.predict(msg_test)))
     print (classification_report(label_test, svm_detector.predict(msg_test)))
 
-    with open('tonDreSent2.pkl', 'wb') as fout:
-        cPickle.dump(svm_detector, fout)
+
+    joblib.dump(svm_detector, 'tweetSent.pkl')
+
+
+
 
 msg_trainp, msg_testp, label_trainp, label_testp = traintest(combi)
 tonyFunction(msg_trainp,msg_testp, label_trainp, label_testp,' Sent')
+
+
+
+
